135e2082fc4840929c3f9cece7a669eb63938ba0
nguyenthucbao <21520617@gm.uit.edu.vn>
2024-09-24 09:24:25 +0700

    Update

---
+++ Assets/Resource/Script/Player.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class Player : MonoBehaviour
+{
+    public Joystick movementJS;
+    public Joystick shootingJS;
+    private Rigidbody2D rb;
+
+    public float speed = 10f;
+    public bool isShooting = false;
+    Vector2 testgame = new Vector2(0, 1);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
+    {
+        if (movementJS.Direction.y != 0)
+        {
+            rb.velocity = new Vector2(movementJS.Direction.x * speed, movementJS.Direction.y * speed);
+            if ( PLayerShooting())
+            {
+                FacingTo(shootingJS.Direction);
+            }
+            else
+            {
+                FacingTo(movementJS.Direction);
+            } 
+                
+            
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            FacingTo(shootingJS.Direction);
+        }
+    }    
+
+    public void FacingTo(Vector2 direction)
+    {
+        //Vector2 direction = movementJS.Direction;
+        if (direction != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    public bool PLayerShooting()
+    {
+        if (shootingJS.Direction.y != 0)          
+        {
+            isShooting = true;
+            return true;
+        } else
+        {
+            isShooting = false;
+            return false;
+        }    
+    }    
+
+}
